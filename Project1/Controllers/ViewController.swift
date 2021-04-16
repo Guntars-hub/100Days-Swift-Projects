a80d3293181326555f7ce310d2c73ee47bf33779
Guntars-hub <61750865+Guntars-hub@users.noreply.github.com>
2021-04-16 16:02:18 +0300

    Project2 and Project3 added
    Project2 and Project3 added

--- Project1/Controllers/ViewController.swift
+++ Project1/Controllers/ViewController.swift
@@ -42,8 +42,8 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
-            let index = indexPath.row
-            vc.title = "Picture \(index + 1) of \(pictures.endIndex)"
+            let startIndex = indexPath.row
+            vc.title = "Picture \(startIndex + 1) of \(pictures.endIndex)"
             navigationController?.pushViewController(vc, animated: true)
         }
     }
