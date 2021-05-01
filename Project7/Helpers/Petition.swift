2a2567607e3738b39fb9376c187cffb81358dbc8
Guntars-hub <61750865+Guntars-hub@users.noreply.github.com>
2021-05-01 15:18:17 +0300

    Project 7 and 8

---
+++ Project7/Helpers/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by guntars.grants on 29/04/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ Project7/Helpers/Petitons.swift
@@ -0,0 +1,12 @@
+//
+//  Petitons.swift
+//  Project7
+//
+//  Created by guntars.grants on 29/04/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
